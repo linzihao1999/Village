16eeaec5fa86b955fd863ff3cf708973e24fb90e
linzihao <104664078@163.com>
2018-11-18 23:17:53 +0800

    add simply leave message activity

--- app/src/main/java/com/linzihao/lx/LeaveMessage.java
+++ app/src/main/java/com/linzihao/lx/LeaveMessage.java
@@ -2,6 +2,9 @@ package com.linzihao.lx;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LeaveMessage extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class LeaveMessage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leave_message);
+        TextView textView = (TextView) findViewById(R.id.textView5);
+        textView.setVisibility(View.INVISIBLE);
+    }
+
+    public void onClick(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText4);
+        String message = editText.getText().toString();
+        TextView textView = (TextView) findViewById(R.id.textView4);
+        TextView textView1 = (TextView) findViewById(R.id.textView5);
+        textView.setText(message);
+        textView1.setVisibility(View.VISIBLE);
     }
 }
