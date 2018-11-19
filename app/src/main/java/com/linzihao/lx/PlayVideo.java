7c1977502535e3de3ce48d9ef8c4b7ea76703031
linzihao <104664078@163.com>
2018-11-19 23:58:30 +0800

    add an activity and somthing

---
+++ app/src/main/java/com/linzihao/lx/PlayVideo.java
@@ -0,0 +1,13 @@
+package com.linzihao.lx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayVideo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_video);
+    }
+}
--- app/src/main/java/com/linzihao/lx/ShowMessage.java
+++ app/src/main/java/com/linzihao/lx/ShowMessage.java
@@ -2,12 +2,21 @@ package com.linzihao.lx;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ShowMessage extends AppCompatActivity {
+    private String[] data = {"Apple", "Banana", "Orange", "Watermelon",
+            "Pear", "Grape", "Pineapple", "Strawberry", "Cherry", "Mango","Lixiang"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_message);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                ShowMessage.this, android.R.layout.simple_list_item_1, data);
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+        super.setTitle("用户留言");
     }
 }
